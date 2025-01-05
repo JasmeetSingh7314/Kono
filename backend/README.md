791af4ecd808a1fb1249a00338ada71bd120cd63
Satvik Srivastava <srivastavasatvik22@gmail.com>
2025-01-05 12:26:37 +0530

    Initial API configuration

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-This directory will contain all the code related to server and db.
---
+++ backend/kono/src/test/java/dev/satvik/kono/KonoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.satvik.kono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
